d2a10570bb7b4cf44914d6abe0605ff0e9240469
moralesja560 <aalberto_morales@hotmail.com>
2022-05-15 09:12:13 -0500

    working update

---
+++ chapter9_video.py
@@ -0,0 +1,30 @@
+# import the opencv library
+import cv2
+faceCascade = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
+  
+# define a video capture object
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, img = vid.read()  
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    faces = faceCascade.detectMultiScale(gray_img, 1.25, 4) 
+    for (x,y,w,h) in faces: 
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)  
+        rec_gray = gray_img[y:y+h, x:x+w] 
+        rec_color = img[y:y+h, x:x+w] 
+    cv2.imshow('Face Recognition',img) 
+   
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
